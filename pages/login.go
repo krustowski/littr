ebc4d9ebf74f03a0099a3c61add664274b483423
krusty <krusty@savla.dev>
2022-12-02 14:17:08 +0100

    add and fix login page responsivness

--- pages/login.go
+++ pages/login.go
@@ -6,12 +6,32 @@ type LoginPage struct {
 	app.Compo
 }
 
+type loginContent struct {
+	app.Compo
+}
+
 func (p *LoginPage) Render() app.UI {
 	return app.Div().Body(
 		app.Body().Class("dark"),
 		&header{},
-		//&loginTable{},
-		app.Div().Class("large-space"),
+		&loginContent{},
 		&footer{},
+	)
+}
+
+func (c *loginContent) Render() app.UI {
+	return app.Main().Class("responsive").Body(
+		app.H5().Text("littr login"),
+		app.P().Text("do not be mid, join us to be lit"),
+
+		app.Div().Class("field label border").Body(
+			app.Input().Type("text"),
+			app.Label().Text("nickname"),
+		),
+		app.Div().Class("field label border").Body(
+			app.Input().Type("password"),
+			app.Label().Text("passphrase"),
+		),
+		app.Button().Class("responsive primary").Text("login"),
 	)
 }
