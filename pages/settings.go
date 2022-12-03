4cc0bfd0e798d9075eacaab17731d78c6b3d59f7
krusty <krusty@savla.dev>
2022-12-03 16:31:50 +0100

    add components to settings page

--- pages/settings.go
+++ pages/settings.go
@@ -24,5 +24,25 @@ func (c *settingsContent) Render() app.UI {
 		app.H5().Text("littr settings"),
 		app.P().Text("change your passphrase, the about string or just fuck off-"),
 		app.Div().Class("space"),
+
+		app.Div().Class("field label border").Body(
+			app.Input().Type("password"),
+			app.Label().Text("passphrase"),
+		),
+		app.Div().Class("field label border").Body(
+			app.Input().Type("password"),
+			app.Label().Text("passphrase again"),
+		),
+		app.Button().Class("responsive primary").Text("change passphrase"),
+		app.Div().Class("large-divider"),
+
+		app.Div().Class("field textarea label border extra").Body(
+			app.Textarea().Text("idk"),
+			app.Label().Text("about"),
+		),
+		app.Button().Class("responsive primary").Text("change about"),
+
+		app.Div().Class("large-space"),
+		app.Div().Class("large-space"),
 	)
 }
