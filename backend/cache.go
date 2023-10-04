d2150c24bb6478bb208d595d270d1f5cffe9391e
Krystof Sara <krystof.sara@wesecon.cz>
2023-10-04 02:04:13 +0200

    preimplement getMany(...) function for pagination

--- backend/cache.go
+++ backend/cache.go
@@ -27,6 +27,15 @@ func getAll[T any](cache *core.Cache, model T) (map[string]T, int) {
 	return items, count
 }
 
+// getMany
+// seek and skip implementation TBD --- pagination idea
+// mark being the ID/key, stop/start the export there in the cache map
+// export the 'count' number of records in map with IDs/keys
+// the 'count' number passthrough to the 2nd return value
+func getMany[T any](cache *core.Cache, mark string, model T, count int) (map[string]T, int) {
+	return make(map[string]T), count
+}
+
 func getOne[T any](cache *core.Cache, key string, model T) (T, bool) {
 	itemInterface, found := cache.Get(key)
 	if !found {
