99c6fce2a3a128dd1be406f7373ab1d7db28603e
Krystof Sara <krystof.sara@wesecon.cz>
2023-10-19 13:44:14 +0200

    fix dump handler's CallerID, refactor auth handler

--- backend/api.go
+++ backend/api.go
@@ -30,20 +30,20 @@ func AuthHandler(w http.ResponseWriter, r *http.Request) {
 
 		reqBody, err := io.ReadAll(r.Body)
 		if err != nil {
-			l.Println(err.Error(), http.StatusInternalServerError)
-
 			resp.Message = "backend error: cannot read input stream"
 			resp.Code = http.StatusInternalServerError
+
+			l.Println(resp.Message+err.Error(), resp.Code)
 			break
 		}
 
 		data := config.Decrypt([]byte(os.Getenv("APP_PEPPER")), reqBody)
 
 		if err = json.Unmarshal(data, &user); err != nil {
-			l.Println(err.Error(), http.StatusInternalServerError)
-
 			resp.Message = "backend error: cannot unmarshall request data"
 			resp.Code = http.StatusInternalServerError
+
+			l.Println(resp.Message+err.Error(), resp.Code)
 			break
 		}
 
@@ -52,18 +52,22 @@ func AuthHandler(w http.ResponseWriter, r *http.Request) {
 		// try to authenticate given user
 		u, ok := authUser(user)
 		if !ok {
-			l.Println("wrong login", http.StatusBadRequest)
-
 			resp.Message = "user not found, or wrong passphrase entered"
 			resp.Code = http.StatusBadRequest
+
+			l.Println(resp.Message, resp.Code)
 			break
 		}
 
 		resp.Users = make(map[string]models.User)
 		resp.Users[u.Nickname] = *u
 		resp.AuthGranted = ok
+
+		resp.Message = "auth granted"
 		resp.Code = http.StatusOK
 		//resp.FlowList = u.FlowListdd
+
+		l.Println(resp.Message, resp.Code)
 		break
 
 	default:
@@ -82,6 +86,7 @@ func DumpHandler(w http.ResponseWriter, r *http.Request) {
 
 	// prepare the Logger instance
 	l := Logger{
+		CallerID:   "system",
 		IPAddress:  r.RemoteAddr,
 		Method:     r.Method,
 		WorkerName: "dump",
