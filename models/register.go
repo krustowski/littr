68f2fdc5036d12116e3a45d43de29e34e43d3cef
Krystof Sara <krystof.sara@wesecon.cz>
2023-10-21 10:21:13 +0200

    remove unused models

--- models/register.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Registration struct {
-	Nickname   string `json:"nickname"`
-	Passphrase string `json:"passphrase"`
-	Poll       int    `json:"poll_id"`
-	Type       string `json:"type"`
-}
