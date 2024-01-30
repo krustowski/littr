51f48a3bb3e7638940897749ccb01372194f6d81
krusty <krusty@savla.dev>
2024-01-30 03:55:57 +0100

    add backend helpers

---
+++ backend/helpers.go
@@ -0,0 +1,9 @@
+package backend
+
+// https://stackoverflow.com/a/34816623
+func reverse(ss []string) {
+	last := len(ss) - 1
+	for i := 0; i < len(ss)/2; i++ {
+		ss[i], ss[last-i] = ss[last-i], ss[i]
+	}
+}
