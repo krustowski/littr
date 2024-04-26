760a6352a802274e2a61cd02dcd955c9b6a897ce
krusty <krusty@savla.dev>
2024-04-26 12:03:49 +0200

    upgrade models.Device model

--- models/subscription.go
+++ models/subscription.go
@@ -22,6 +22,12 @@ type Device struct {
 	// Timestamp of the subscription creation.
 	TimeCreated time.Time `json:"time_created"`
 
+	// Timestamp of the last notification sent through this device.
+	TimeLastUsed time.Time `json:"time_last_used"`
+
+	// List of labels for such device.
+	Tags []string `json:"tags,omitempty"`
+
 	// The very subscription struct/details.
 	//Subscription app.NotificationSubscription `json:"subscription"`
 	Subscription webpush.Subscription `json:"subscription"`
