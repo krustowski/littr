72ee6d0517a0b5b72b12e969e7013480c2e00a18
krustowski <krusty@savla.dev>
2024-04-27 01:14:37 +0100

    update models

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -73,3 +73,21 @@ type User struct {
 	// Tags is an array of possible roles and other various attributes assigned to such user.
 	Tags []string `json:"tags"`
 }
+
+// UserStat is a helper struct to hold statistics about the whole app.
+type UserStat struct {
+	// PostCount is a number of posts of such user.
+	PostCount int `default:0`
+
+	// ReactionCount tells the number of interactions (stars given).
+	ReactionCount int `default:0`
+
+	// FlowerCount is basically a number of followers.
+	FlowerCount int `default:0`
+
+	// ShadeCount is basically a number of blockers.
+	ShadeCount int `default:0`
+
+	// Searched is a special boolean used by the search engine to mark who is to be shown in search results.
+	Searched bool `default:true`
+}
