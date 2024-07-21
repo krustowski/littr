edfbef757ccd13aaa0f0da52515f0673d559585a
krustowski <krusty@savla.dev>
2024-07-21 15:33:39 +0200

    update docs

--- pkg/backend/router.go
+++ pkg/backend/router.go
@@ -1,5 +1,5 @@
 // @title		litter-go
-// @version	 	0.35.22
+// @version	 	0.35.23
 // @description		nanoblogging platform as PWA built on go-app framework
 // @termsOfService	https://littr.eu/tos
 
