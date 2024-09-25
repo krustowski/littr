c8cd0e4a12e733d98279ca34da6a773c51a09b11
krustowski <krusty@savla.dev>
2024-09-25 02:08:40 +0200

    dehardcode app's main URL in mails

--- pkg/backend/users/controllers.go
+++ pkg/backend/users/controllers.go
@@ -1128,17 +1128,21 @@ func composeResetMail(payload msgPayload) (*gomail.Msg, error) {
 		return nil, err
 	}
 
+	mainURL := os.Getenv("APP_URL_MAIN")
+	if mainURL == "" {
+		mainURL = "www.littr.eu"
+	}
+
 	switch payload.Type {
 	case "request":
 		if payload.UUID == "" {
 			return nil, fmt.Errorf("no UUID given for mail composition")
 		}
 
-		// TODO: do not hardcode this
-		resetLink := "https://www.littr.eu/reset/" + payload.UUID
+		resetLink := "https://" + mainURL + "/reset/" + payload.UUID
 
 		m.Subject("Passphrase Reset Request")
-		m.SetBodyString(gomail.TypeTextPlain, "Dear user,\n\nWe received a request to reset the passphrase for your account associated with this e-mail address: "+payload.Email+"\n\nTo reset your passphrase, please click the link below:\n\nReset Passphrase Link: "+resetLink+"\n\nYou can insert the generated UUID in the reset form too: "+payload.UUID+"\n\nIf you did not request a passphrase reset, please ignore this email. Your passphrase will remain unchanged.\n\nFor security reasons, this link will expire in 24 hours.\n\nThank you\nhttps://www.littr.eu")
+		m.SetBodyString(gomail.TypeTextPlain, "Dear user,\n\nWe received a request to reset the passphrase for your account associated with this e-mail address: "+payload.Email+"\n\nTo reset your passphrase, please click the link below:\n\nReset Passphrase Link: "+resetLink+"\n\nYou can insert the generated UUID in the reset form too: "+payload.UUID+"\n\nIf you did not request a passphrase reset, please ignore this email. Your passphrase will remain unchanged.\n\nFor security reasons, this link will expire in 24 hours.\n\nThank you\nlittr\nhttps://"+mainURL)
 
 	case "passphrase":
 		if payload.Passphrase == "" {
@@ -1146,7 +1150,7 @@ func composeResetMail(payload msgPayload) (*gomail.Msg, error) {
 		}
 
 		m.Subject("Your New Passphrase")
-		m.SetBodyString(gomail.TypeTextPlain, "Dear user,\n\nThe requested passphrase regeneration process has been successful. Please use the generated string below to log-in again.\n\nNew passphrase: "+payload.Passphrase+"\n\nPlease do not forget to change the passphrase right after logging in in settings.\n\nThank you\nhttps://www.littr.eu")
+		m.SetBodyString(gomail.TypeTextPlain, "Dear user,\n\nThe requested passphrase regeneration process has been successful. Please use the generated string below to log-in again.\n\nNew passphrase: "+payload.Passphrase+"\n\nPlease do not forget to change the passphrase right after logging in in settings.\n\nThank you\nlittr\nhttps://"+mainURL)
 
 	default:
 		return nil, fmt.Errorf("no mail Type specified")
