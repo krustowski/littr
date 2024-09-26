da39cd786379e4709c67265f59e14ba8a3dc95b4
krustowski <krusty@savla.dev>
2024-09-26 03:49:21 +0200

    prepare universal paginator, refactor flow paginator (wip)

--- pkg/backend/pages/common.go
+++ pkg/backend/pages/common.go
@@ -2,28 +2,28 @@
 package pages
 
 import (
-	"sort"
-	"strings"
-
 	"go.vxn.dev/littr/pkg/backend/db"
-	"go.vxn.dev/littr/pkg/helpers"
 	"go.vxn.dev/littr/pkg/models"
 )
 
 const PAGE_SIZE int = 25
 
+// DTO for GetOnePage input aggregation
 type PageOptions struct {
+	// common options
+	Caller   models.User
 	CallerID string          `json:"caller_id"`
 	PageNo   int             `json:"page_no"`
 	FlowList map[string]bool `json:"folow_list"`
 
-	Flow  flowOptions `json:"flow_args"`
-	Polls pollOptions `json:"poll_args"`
-	Users userOptions `json:"user_args"`
+	// data compartments' specifications
+	Flow  flowOptions `json:"flow_options"`
+	Polls pollOptions `json:"poll_options"`
+	Users userOptions `json:"user_options"`
 }
 
+// flow subviews' options
 type flowOptions struct {
-	// flow subroutes' booleans and vars
 	SinglePost   bool   `json:"single_post"`
 	UserFlow     bool   `json:"user_flow"`
 	SinglePostID string `json:"single_post_id"`
@@ -32,14 +32,14 @@ type flowOptions struct {
 	HideReplies  bool   `json:"hide_replies"`
 }
 
+// polls subviews' options
 type pollOptions struct {
-	// polls
 	SinglePoll   bool   `json:"single_poll"`
 	SinglePollID string `json:"single_poll_id"`
 }
 
+// users subviews' options
 type userOptions struct {
-	// users
 	SingleUser   bool   `json:"single_user"`
 	SingleUserID string `json:"single_user_id"`
 }
@@ -58,16 +58,18 @@ type PagePointers struct {
 	PtrUsers *map[string]models.User
 }
 
-// fillMaps is a function, that prepares raw maps of all (related) items for further processing according to input options
-func fillMaps(opts PageOptions) *maps {
-	if opts.Flow != nil {
+// fillDataMaps is a function, that prepares raw maps of all (related) items for further processing according to input options
+func fillDataMaps(opts PageOptions) *maps {
+	// prepare data maps for a flow page
+	if opts.Flow != (flowOptions{}) {
 		posts, _ := db.GetAll(db.FlowCache, models.Post{})
 		users, _ := db.GetAll(db.UserCache, models.User{})
 
 		return &maps{Posts: posts, Users: users}
 	}
 
-	if opts.Polls != nil {
+	// prepare data map for a polls page
+	if opts.Polls != (pollOptions{}) {
 		polls, _ := db.GetAll(db.PollCache, models.Poll{})
 		// users are not needed necessarily there for now...
 		//users, _ := db.GetAll(db.UserCache, models.User{})
@@ -75,7 +77,8 @@ func fillMaps(opts PageOptions) *maps {
 		return &maps{Polls: polls}
 	}
 
-	if opts.Polls != nil {
+	// prepare data map for a users page
+	if opts.Users != (userOptions{}) {
 		users, _ := db.GetAll(db.UserCache, models.User{})
 
 		return &maps{Users: users}
@@ -84,16 +87,15 @@ func fillMaps(opts PageOptions) *maps {
 	return nil
 }
 
-func GetOnePage(opts PageOptions) PagePointers { 
+func GetOnePage(opts PageOptions) PagePointers {
 	// validate the callerID is a legitimate user's ID
-	caller, ok := db.GetOne(db.UserCache, opts.CallerID, models.User{})
-	if !ok {
+	if opts.Caller, ok := db.GetOne(db.UserCache, opts.CallerID, models.User{}); !ok {
 		// unregistred caller
 		return nil
 	}
 
 	// pointer to maps of all items (based on and related to the opts input)
-	ptrMaps := fillMaps(opts)
+	ptrMaps := fillDataMaps(opts)
 	if ptrMaps == nil {
 		// invalid input options = resulted in empty maps only
 		return ptrMaps
@@ -104,166 +106,14 @@ func GetOnePage(opts PageOptions) PagePointers {
 	}
 
 	if opts.Polls != nil {
-		return onePagePolls(opts, ptrMaps)
+		// NYI
+		//return onePagePolls(opts, ptrMaps)
 	}
 
 	if opts.Users != nil {
-		return onePageUsers(opts, ptrMaps)
+		// NYI
+		//return onePageUsers(opts, ptrMaps)
 	}
 
 	return nil
-}
-
-	// BTW those variables are both of type map[string]T
-	var allPolls map[string]models.Poll
-	var allPosts map[string]models.Post
-	var allUsers map[string]models.User
-
-
-	// pagination draft
-	// + only select N latest posts for such user according to their FlowList
-	// + include previous posts to a reply
-	// + only include users mentioned
-
-	posts := []models.Post{}
-	num := 0
-
-	// overload flowList
-	flowList := caller.FlowList
-	if opts.FlowList != nil {
-		flowList = opts.FlowList
-	}
-
-	// assign reply count to each post
-	for _, post := range allPosts {
-		if post.ReplyToID == "" {
-			continue
-		}
-
-		// calculate the reply count for each post
-		origo, found := allPosts[post.ReplyToID]
-		if found {
-			origo.ReplyCount++
-			allPosts[origo.ID] = origo
-		}
-	}
-
-	// filter out all posts for such callerID
-	for _, post := range allPosts {
-		// check the caller's flow list, skip on unfollowed, or unknown user
-		if value, found := flowList[post.Nickname]; !found || !value {
-			continue
-		}
-
-		if opts.Hashtag != "" {
-			if strings.Contains(post.Content, "#"+opts.Hashtag) {
-				posts = append(posts, post)
-			}
-			continue
-		}
-
-		// filter replies out
-		if opts.HideReplies && post.ReplyToID != "" {
-			continue
-		}
-
-		// exctract replies to the single post
-		if opts.SinglePost && opts.SinglePostID != "" {
-			if post.ReplyToID == opts.SinglePostID || post.ID == opts.SinglePostID {
-				posts = append(posts, post)
-			}
-			continue
-		}
-
-		if opts.UserFlow && opts.UserFlowNick != "" {
-			if value, found := user.FlowList[opts.UserFlowNick]; (!value || !found) && allUsers[opts.UserFlowNick].Private {
-				continue
-			}
-
-			if post.Nickname == opts.UserFlowNick {
-				posts = append(posts, post)
-			}
-			continue
-		}
-
-		posts = append(posts, post)
-	}
-
-	// order posts by timestamp DESC
-	sort.SliceStable(posts, func(i, j int) bool {
-		return posts[i].Timestamp.After(posts[j].Timestamp)
-	})
-
-	// cut the <pageSize>*2 number of posts only
-	var part []models.Post
-
-	pageNo := opts.PageNo
-	start := (PAGE_SIZE * 2) * pageNo
-	end := (PAGE_SIZE * 2) * (pageNo + 1)
-
-	if len(posts) > start {
-		// only valid for the very first page
-		//part = posts[0:(pageSize * 2)]
-
-		if len(posts) <= end {
-			// the very last page
-			part = posts[start:]
-		} else {
-			// the middle page
-			part = posts[start:end]
-		}
-	} else {
-		// the very single page
-		part = posts
-	}
-
-	// loop through the array and manually include other posts too
-	// watch for users as well
-	pExport := make(map[string]models.Post)
-	uExport := make(map[string]models.User)
-
-	num = 0
-	for _, post := range part {
-		// increase the count of posts
-		num++
-
-		// export one (1) post
-		pExport[post.ID] = post
-		uExport[post.Nickname] = allUsers[post.Nickname]
-
-		// we can have multiple keys from a single post -> its interractions
-		repKey := post.ReplyToID
-		if repKey != "" {
-			if prePost, found := allPosts[repKey]; found {
-				num++
-
-				// export previous user too
-				nick := prePost.Nickname
-				uExport[nick] = allUsers[nick]
-
-				// mange private content
-				if value, found := user.FlowList[nick]; (!value || !found) && allUsers[nick].Private {
-					prePost.Content = ""
-				}
-
-				// increase the reply count
-				prePost.ReplyCount++
-				pExport[repKey] = prePost
-			}
-		}
-
-		// this makes sure only N posts are returned, but it cuts off the tail posts
-		/*if num > pageSize {
-			break
-		}*/
-	}
-
-	// ensure the UserFlowNick is always included too
-	if _, found := uExport[opts.UserFlowNick]; !found {
-		if _, found = allUsers[opts.UserFlowNick]; found {
-			uExport[opts.UserFlowNick] = allUsers[opts.UserFlowNick]
-		}
-	}
-
-	return pExport, uExport
 }
