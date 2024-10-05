6a0a8b7fdc757c22afd660c4b319bada3210f2b4
krustowski <krusty@savla.dev>
2024-10-05 20:01:32 +0200

    refactor welcome FE, update docs

--- pkg/backend/router.go
+++ pkg/backend/router.go
@@ -1,5 +1,5 @@
 // @title		littr
-// @version	 	0.41.2
+// @version	 	0.41.3
 // @description		a simple nanoblogging platform as PWA built on go-app framework
 // @termsOfService	https://www.littr.eu/tos
 
