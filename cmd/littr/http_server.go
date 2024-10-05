fbccb3030d9901486c113d0b6663301a1429dbd8
krustowski <krusty@savla.dev>
2024-10-05 18:55:26 +0200

    refactor settings FE (wip)

--- cmd/littr/http_server.go
+++ cmd/littr/http_server.go
@@ -39,7 +39,7 @@ func initClient() {
 	app.Route("/register", &fe.RegisterPage{})
 	app.Route("/reset", &fe.ResetPage{})
 	app.RouteWithRegexp("/reset/\\w+", &fe.ResetPage{})
-	app.Route("/settings", &fe.SettingsPage{})
+	app.Route("/settings", &fe.SettingsView{})
 	app.Route("/stats", &fe.StatsPage{})
 	app.Route("/tos", &fe.ToSPage{})
 	app.Route("/users", &fe.UsersView{})
--- pkg/frontend/views.go
+++ pkg/frontend/views.go
@@ -2,6 +2,7 @@ package frontend
 
 import (
 	"go.vxn.dev/littr/pkg/frontend/polls"
+	"go.vxn.dev/littr/pkg/frontend/settings"
 	"go.vxn.dev/littr/pkg/frontend/users"
 
 	"github.com/maxence-charriere/go-app/v9/pkg/app"
@@ -25,6 +26,30 @@ func (v *PollsView) Render() app.UI {
 		&footer{},
 		&polls.Content{},
 	)
+}
+
+/*
+ *  settings
+ */
+
+type SettingsView struct {
+	app.Compo
+
+	mode string
+}
+
+func (v *SettingsView) Render() app.UI {
+	return app.Div().Body(
+		&header{},
+		&footer{},
+		&settings.Content{},
+	)
+}
+
+func (v *SettingsView) OnNav(ctx app.Context) {
+	ctx.Page().SetTitle("settings / littr")
+
+	ctx.LocalStorage().Get("mode", &v.mode)
 }
 
 /*
