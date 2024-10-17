b6786812309e99339d0b299081232592d2d67744
krustowski <krusty@savla.dev>
2024-10-17 21:18:04 +0200

    fix push controllers and docs

--- pkg/backend/push/controllers.go
+++ pkg/backend/push/controllers.go
@@ -11,6 +11,7 @@ import (
 	"go.vxn.dev/littr/pkg/helpers"
 	"go.vxn.dev/littr/pkg/models"
 
+	chi "github.com/go-chi/chi/v5"
 	"github.com/maxence-charriere/go-app/v9/pkg/app"
 )
 
@@ -21,30 +22,36 @@ import (
 // @Tags         push
 // @Accept       json
 // @Produce      json
-// @Success      200  {object}   common.APIResponse
-// @Failure 	 401  {object}   common.APIResponse
+// @Success      200  {object}   common.APIResponse{data=push.getchVAPIDKey.responseData}
+// @Failure 	 400  {object}   common.APIResponse
+// @Failure 	 401  {object}   common.APIResponse{data=auth.authHandler.responseData}
 // @Router       /push/vapid [get]
 func fetchVAPIDKey(w http.ResponseWriter, r *http.Request) {
 	l := common.NewLogger(r, "push")
 
+	type responseData struct {
+		Key string `json:"key"`
+	}
+
+	// fetch the caller's nickname (callerID)
 	callerID, ok := r.Context().Value("nickname").(string)
 	if !ok {
 		l.Msg(common.ERR_CALLER_FAIL).Status(http.StatusBadRequest).Log().Payload(nil).Write(w)
 		return
 	}
 
+	// skip blank callerID
 	if callerID == "" {
 		l.Msg(common.ERR_CALLER_BLANK).Status(http.StatusBadRequest).Log().Payload(nil).Write(w)
 		return
 	}
 
-	pl := struct {
-		Key string `json:"key"`
-	}{
+	// prepare the payload
+	pl := &responseData{
 		Key: os.Getenv("VAPID_PUB_KEY"),
 	}
 
-	l.Msg("ok, sending public VAPID key").Status(http.StatusOK).Log().Payload(&pl).Write(w)
+	l.Msg("ok, sending public VAPID key").Status(http.StatusOK).Log().Payload(pl).Write(w)
 	return
 }
 
@@ -55,25 +62,32 @@ func fetchVAPIDKey(w http.ResponseWriter, r *http.Request) {
 // @Tags         push
 // @Accept       json
 // @Produce      json
+// @Param        uuid path string true "UUID of a device to update"
 // @Success      200  {object}   common.APIResponse
-// @Failure      409  {object}   common.APIResponse
+// @Failure      400  {object}   common.APIResponse
 // @Failure      500  {object}   common.APIResponse
 // @Router       /push/subscription/{uuid}/mention [put]
 // @Router       /push/subscription/{uuid}/reply [put]
 func updateSubscription(w http.ResponseWriter, r *http.Request) {
 	l := common.NewLogger(r, "push")
 
+	// fetch the caller's nickname
 	callerID, ok := r.Context().Value("nickname").(string)
 	if !ok {
 		l.Msg(common.ERR_CALLER_FAIL).Status(http.StatusBadRequest).Log().Payload(nil).Write(w)
 		return
 	}
 
-	path := r.URL.Path
-	payload := models.Device{}
+	// skip blank callerID
+	if callerID == "" {
+		l.Msg(common.ERR_CALLER_BLANK).Status(http.StatusBadRequest).Log().Payload(nil).Write(w)
+		return
+	}
 
-	if err := common.UnmarshalRequestData(r, &payload); err != nil {
-		l.Msg(common.ERR_INPUT_DATA_FAIL).Status(http.StatusBadRequest).Error(err).Log().Payload(nil).Write(w)
+	// take the param from path
+	uuid := chi.URLParam(r, "uuid")
+	if uuid == "" {
+		l.Msg(common.ERR_PUSH_UUID_BLANK).Status(http.StatusBadRequest).Log().Payload(nil).Write(w)
 		return
 	}
 
@@ -85,41 +99,66 @@ func updateSubscription(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	tag := ""
+	var device models.Device
+
+	// get the specified device by its UUID
+	for _, dev := range devs {
+		if dev.UUID == uuid {
+			device = dev
+		}
+	}
+
+	// "switch" the subscription type
+	path := r.URL.Path
+
+	var tag string
+
+	// decide which tag we are processing at the moment
 	if strings.Contains(path, "mention") {
 		tag = "mention"
 	} else if strings.Contains(path, "reply") {
 		tag = "reply"
 	}
 
-	if !helpers.Contains(payload.Tags, tag) {
-		payload.Tags = append(payload.Tags, tag)
+	// append the tag if not present already
+	if !helpers.Contains(device.Tags, tag) {
+		device.Tags = append(device.Tags, tag)
 	} else {
-		tags := payload.Tags
-		newTags := []string{}
+		// remove the existing tag
+		tags := device.Tags
+
+		var newTags []string
+
+		// loop over tags and yeet the one existing and requested
 		for _, t := range tags {
 			if t == tag {
 				continue
 			}
 			newTags = append(newTags, t)
 		}
-		payload.Tags = newTags
+
+		// update device's tags
+		device.Tags = newTags
 	}
 
-	// if found, update the existing device
+	// loop through loaded devices and look for the requested one
+	// if found, update the existing requested device
 	found := false
 	for idx, dev := range devs {
-		if dev.UUID == payload.UUID {
-			devs[idx] = payload
+		// requested dev's UUID matches loaded device's UUID
+		if dev.UUID == device.UUID {
+			// save the requested device to the device array
+			devs[idx] = device
 			found = true
 		}
 	}
 
-	// append new device into the devices array for such user
+	// append new device into the devices array for such user, if not found before
 	if !found {
-		devs = append(devs, payload)
+		devs = append(devs, device)
 	}
 
+	// save the updated device array back to the database
 	if saved := db.SetOne(db.SubscriptionCache, callerID, devs); !saved {
 		l.Msg(common.ERR_SUBSCRIPTION_SAVE_FAIL).Status(http.StatusInternalServerError).Log().Payload(nil).Write(w)
 		return
@@ -136,22 +175,32 @@ func updateSubscription(w http.ResponseWriter, r *http.Request) {
 // @Tags         push
 // @Accept       json
 // @Produce      json
+// @Param    	 request body models.Device true "device to subscribe"
 // @Success      200  {object}   common.APIResponse
+// @Failure      400  {object}   common.APIResponse
 // @Failure      409  {object}   common.APIResponse
 // @Failure      500  {object}   common.APIResponse
 // @Router       /push/subscription [post]
 func subscribeToNotifications(w http.ResponseWriter, r *http.Request) {
 	l := common.NewLogger(r, "push")
 
+	// fetch the caller's nickname
 	callerID, ok := r.Context().Value("nickname").(string)
 	if !ok {
 		l.Msg(common.ERR_CALLER_FAIL).Status(http.StatusBadRequest).Log().Payload(nil).Write(w)
 		return
 	}
 
-	payload := models.Device{}
+	// skip blank callerID
+	if callerID == "" {
+		l.Msg(common.ERR_CALLER_BLANK).Status(http.StatusBadRequest).Log().Payload(nil).Write(w)
+		return
+	}
 
-	if err := common.UnmarshalRequestData(r, &payload); err != nil {
+	var device models.Device
+
+	// decode the incoming request's body
+	if err := common.UnmarshalRequestData(r, &device); err != nil {
 		l.Msg(common.ERR_INPUT_DATA_FAIL).Status(http.StatusBadRequest).Error(err).Log().Payload(nil).Write(w)
 		return
 	}
@@ -164,17 +213,19 @@ func subscribeToNotifications(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// loop through the devices and check its presence (present = already subscribed)
 	for _, dev := range devs {
-		if dev.UUID == payload.UUID {
+		if dev.UUID == device.UUID {
 			// we have just found a match, thus request was fired twice, or someone tickles the API
-			l.Msg(common.ERR_DEVICE_SUBSCRIBED).Status(http.StatusConflict).Log().Payload(nil).Write(w)
+			l.Msg(common.ERR_DEVICE_SUBSCRIBED_ALREADY).Status(http.StatusConflict).Log().Payload(nil).Write(w)
 			return
 		}
 	}
 
 	// append new device into the devices array for such user
-	devs = append(devs, payload)
+	devs = append(devs, device)
 
+	// save the device array back to the database
 	if saved := db.SetOne(db.SubscriptionCache, callerID, devs); !saved {
 		l.Msg(common.ERR_SUBSCRIPTION_SAVE_FAIL).Status(http.StatusInternalServerError).Log().Payload(nil).Write(w)
 		return
@@ -189,11 +240,11 @@ func subscribeToNotifications(w http.ResponseWriter, r *http.Request) {
 // @Summary      Send a notification
 // @Description  Send a notification
 // @Tags         push
-// @Accept       json
 // @Produce      json
+// @Param        postID path string true "original post's ID"
 // @Success      200  {object}   common.APIResponse
-// @Success      201  {object}   common.APIResponse
-// @Failure      400  {object}   common.APIResponse
+// @Success      400  {object}   common.APIResponse
+// @Failure      404  {object}   common.APIResponse
 // @Router       /push/notification/{postID} [post]
 func sendNotification(w http.ResponseWriter, r *http.Request) {
 	l := common.NewLogger(r, "push")
@@ -205,22 +256,27 @@ func sendNotification(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	payload := struct {
-		OriginalID string `json:"original_post"`
-	}{}
-
-	if err := common.UnmarshalRequestData(r, &payload); err != nil {
-		l.Msg(common.ERR_INPUT_DATA_FAIL).Status(http.StatusBadRequest).Error(err).Log().Payload(nil).Write(w)
+	// skip blank callerID
+	if callerID == "" {
+		l.Msg(common.ERR_CALLER_BLANK).Status(http.StatusBadRequest).Log().Payload(nil).Write(w)
 		return
 	}
 
-	// fetch related data from cachces
-	post, ok := db.GetOne(db.FlowCache, payload.OriginalID, models.Post{})
+	// take the param from path
+	postID := chi.URLParam(r, "postID")
+	if postID == "" {
+		l.Msg(common.ERR_POSTID_BLANK).Status(http.StatusBadRequest).Log().Payload(nil).Write(w)
+		return
+	}
+
+	// fetch related data from the database
+	post, ok := db.GetOne(db.FlowCache, postID, models.Post{})
 	if !ok {
 		l.Msg(common.ERR_POST_NOT_FOUND).Status(http.StatusNotFound).Log().Payload(nil).Write(w)
 		return
 	}
 
+	// fetch notification receiver's device list
 	devs, ok := db.GetOne(db.SubscriptionCache, post.Nickname, []models.Device{})
 	if !ok {
 		// it is OK for this to return nothing, loop can handle it later...
@@ -267,58 +323,65 @@ func sendNotification(w http.ResponseWriter, r *http.Request) {
 // @Summary      Delete a subscription
 // @Description  delete a subscription
 // @Tags         push
-// @Accept       json
 // @Produce      json
+// @Param        uuid path string true "UUID of a device to delete"
 // @Success      200  {object}   common.APIResponse
 // @Failure      400  {object}   common.APIResponse
+// @Failure      404  {object}   common.APIResponse
 // @Failure      500  {object}   common.APIResponse
 // @Router       /push/subscription/{uuid} [delete]
 func deleteSubscription(w http.ResponseWriter, r *http.Request) {
 	l := common.NewLogger(r, "push")
 
+	// fetch the caller's nickname
 	callerID, ok := r.Context().Value("nickname").(string)
 	if !ok {
 		l.Msg(common.ERR_CALLER_FAIL).Status(http.StatusBadRequest).Log().Payload(nil).Write(w)
 		return
 	}
 
-	payload := struct {
-		UUID string `json:"device_uuid"`
-	}{}
-
-	if err := common.UnmarshalRequestData(r, &payload); err != nil {
-		l.Msg(common.ERR_INPUT_DATA_FAIL).Status(http.StatusBadRequest).Error(err).Log().Payload(nil).Write(w)
+	// skip blank callerID
+	if callerID == "" {
+		l.Msg(common.ERR_CALLER_BLANK).Status(http.StatusBadRequest).Log().Payload(nil).Write(w)
 		return
 	}
 
-	uuid := payload.UUID
+	// take the param from path
+	uuid := chi.URLParam(r, "uuid")
 	if uuid == "" {
 		l.Msg(common.ERR_PUSH_UUID_BLANK).Status(http.StatusBadRequest).Log().Payload(nil).Write(w)
 		return
 	}
 
-	devices, ok := db.GetOne(db.SubscriptionCache, callerID, []models.Device{})
+	// let us check this device
+	// we are about to loop through []models.Device fetched from SubscriptionCache later on
+	devs, ok := db.GetOne(db.SubscriptionCache, callerID, []models.Device{})
 	if !ok {
-		l.Msg(common.ERR_SUBSCRIPTION_NOT_FOUND).Status(http.StatusNotFound).Log().Payload(nil).Write(w)
+		l.Msg(common.ERR_DEVICE_NOT_FOUND).Status(http.StatusNotFound).Log().Payload(nil).Write(w)
 		return
 	}
 
+	// prepare a new array for remaining devices
 	var newDevices []models.Device
 
-	for _, dev := range devices {
+	// loop through fetched devices, skip the requested one as well as the blank ones
+	for _, dev := range devs {
 		if dev.UUID == uuid {
 			// do not include this device anymore
 			continue
 		}
 
+		//if dev.UUID == "" || dev == (models.Device{}) {
 		if dev.UUID == "" {
-			// do not include blank-labeled devices
+			// do not include blank-labeled devices or blank ones in general
 			continue
 		}
 
+		// append non matching device
 		newDevices = append(newDevices, dev)
 	}
 
+	// save the update device list
 	if saved := db.SetOne(db.SubscriptionCache, callerID, newDevices); !saved {
 		l.Msg(common.ERR_SUBSCRIPTION_SAVE_FAIL).Status(http.StatusInternalServerError).Log().Payload(nil).Write(w)
 		return
