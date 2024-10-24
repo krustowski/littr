44e673d5f0706408dd9024a9e8b5154cd367b0d0
krustowski <krusty@savla.dev>
2024-10-24 20:13:44 +0200

    upgrade dockerfile frontend version, fix typo

--- pkg/frontend/navbars/render.go
+++ pkg/frontend/navbars/render.go
@@ -111,7 +111,7 @@ func (h *Header) Render() app.UI {
 					app.H4().Title("system info (click to open)").Class("center-align deep-orange-text").OnClick(h.onClickHeadline).ID("top-header").Body(
 						app.Span().Body(
 							app.Text(headerString),
-							app.If(app.Getenv("APP_ENVIRONNMENT") != "prod",
+							app.If(app.Getenv("APP_ENVIRONMENT") != "prod",
 								app.Span().Class("col").Body(
 									app.Sup().Body(
 										app.Text(" (dev) "),
@@ -148,7 +148,7 @@ func (h *Header) Render() app.UI {
 							app.H4().Body(
 								app.Span().Body(
 									app.Text("littr"),
-									app.If(app.Getenv("APP_ENVIRONNMENT") != "prod",
+									app.If(app.Getenv("APP_ENVIRONMENT") != "prod",
 										app.Span().Class("col").Body(
 											app.Sup().Body(
 												app.Text(" (dev) "),
