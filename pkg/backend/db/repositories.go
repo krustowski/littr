af48071b53316fe73822bf343b35e61c6ead5669
krustowski <krusty@savla.dev>
2024-11-02 21:40:43 +0100

    add service and repository interfaces to models pkg, add users and posts repositories

--- pkg/backend/db/repositories.go
+++ pkg/backend/db/repositories.go
@@ -5,41 +5,13 @@ import (
 )
 
 //
-//  Repository interfaces
-//
-
-type PollRepositoryInterface interface {
-	GetAll(interface{}) (*map[string]models.Poll, error)
-	GetByID(pollID string) (*models.Poll, error)
-	Save(poll *models.Poll) error
-	//Update(poll *models.Poll) error
-	Delete(pollID string) error
-}
-
-type PostRepositoryInterface interface {
-	GetAll(interface{}) (*map[string]models.Post, error)
-	GetByID(postID string) (*models.Post, error)
-	Save(post *models.Post) error
-	//Update(post *models.Post) error
-	Delete(postID string) error
-}
-
-type UserRepositoryInterface interface {
-	GetAll(interface{}) (*map[string]models.User, error)
-	GetByID(userID string) (*models.User, error)
-	Save(user *models.User) error
-	//Update(user *models.User) error
-	Delete(userID string) error
-}
-
-//
 //  Repositories
 //
 
 type Repositories struct {
-	PollRepository PollRepositoryInterface
-	PostRepository PostRepositoryInterface
-	UserRepository UserRepositoryInterface
+	PollRepository models.PollRepositoryInterface
+	PostRepository models.PostRepositoryInterface
+	UserRepository models.UserRepositoryInterface
 }
 
 var Storage *Repositories
