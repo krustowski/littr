35e569d61bb2cba889b0da21cb48af683d1ae43e
krustowski <krusty@savla.dev>
2024-11-12 23:27:10 +0100

    improve other toast invocations, indicated that the toast is frozen, disable compression

--- pkg/backend/router.go
+++ pkg/backend/router.go
@@ -1,5 +1,5 @@
 // @title		littr
-// @version	 	0.44.45
+// @version	 	0.44.46
 // @description		a simple nanoblogging platform as PWA built on go-app framework
 // @termsOfService	https://www.littr.eu/tos
 
