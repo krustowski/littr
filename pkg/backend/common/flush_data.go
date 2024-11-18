ade60b02090dfc5616b15345bd93188f93e1ba02
krustowski <krusty@savla.dev>
2024-11-18 03:28:18 +0100

    update common.FlushUserData to flush user's options map too

--- pkg/backend/common/flush_data.go
+++ pkg/backend/common/flush_data.go
@@ -4,27 +4,29 @@ import (
 	"go.vxn.dev/littr/pkg/models"
 )
 
-// helper function to flush sensitive user data in the export for response
+// Helper function to flush sensitive user data in the export for response.
 func FlushUserData(users *map[string]models.User, callerID string) *map[string]models.User {
 	if users == nil || callerID == "" {
 		return nil
 	}
 
-	// flush unwanted properties
+	// Flush unwanted properties.
 	for key, user := range *users {
 		user.Passphrase = ""
 		user.PassphraseHex = ""
 
-		// these are kept for callerID
+		// These are kept for callerID.
 		if user.Nickname != callerID {
 			user.Email = ""
 			user.FlowList = nil
 			user.ShadeList = nil
 
-			// TODO map of user's options
-			//user.Options = nil
+			// Flush user's options, keep the private state only.
+			options := map[string]bool{}
+			options["private"] = user.Options["private"]
+			user.Options = options
 
-			// return the caller's status in counterpart account's req. list only callerID's state if present
+			// Return the caller's status in counterpart account's req. list only callerID's state if present.
 			if value, found := user.RequestList[callerID]; found {
 				user.RequestList = make(map[string]bool)
 				user.RequestList[callerID] = value
