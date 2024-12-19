829c2544c976224a7e0ec709635876691a23ccab
krusty <krusty@vxn.dev>
2024-12-19 11:57:01 +0100

    increase instance memory limit to 512 MB

--- pkg/backend/router.go
+++ pkg/backend/router.go
@@ -1,5 +1,5 @@
 // @title		littr
-// @version	 	0.45.16
+// @version	 	0.45.17
 // @description		a simple nanoblogging platform as PWA built on go-app framework
 // @termsOfService	https://www.littr.eu/tos
 
