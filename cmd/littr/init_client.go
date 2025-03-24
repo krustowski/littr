3258a27e24168657f5e297318323af5c28167eaa
krusty <krusty@vxn.dev>
2025-03-24 20:17:14 +0100

    fix regexp route to reset UUID format

--- cmd/littr/init_client.go
+++ cmd/littr/init_client.go
@@ -41,7 +41,7 @@ func initClientCommon() {
 	app.Route("/reset", func() app.Composer {
 		return &fe.ResetView{}
 	})
-	app.RouteWithRegexp("^/reset/[a-zA-Z]+$", func() app.Composer {
+	app.RouteWithRegexp("^/reset/[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}$", func() app.Composer {
 		return &fe.ResetView{}
 	})
 	app.Route("/settings", func() app.Composer {
